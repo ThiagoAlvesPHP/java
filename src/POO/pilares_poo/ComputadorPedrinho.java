68f83872d61cf74b648c099a5ebe9b29de6ab0a0
ThiagoAlvesPHP <thiagoalves@ltdeveloper.com.br>
2024-10-29 15:31:50 -0300

    feature: encapsulamento

---
+++ src/POO/pilares_poo/ComputadorPedrinho.java
@@ -0,0 +1,10 @@
+package POO.pilares_poo;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        //abrindo MSN Messenger
+        MSNMessenger msn = new MSNMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
